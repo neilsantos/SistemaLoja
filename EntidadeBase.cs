77892f17dedcba23855a9075a0d4db31bc91fb7b
NeilAngelo <neil.angelo@hotmail.com>
2020-08-19 09:21:14 -0300

    Hello World!

---
+++ EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaLoja
+{
+    class EntidadeBase
+    {
+
+        int id;
+
+    }
+}
---
+++ Produtos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaLoja
+{
+    class Produtos:EntidadeBase
+    {
+        string nome;
+        Categoria categoria;
+        Marca marca;
+        float valorpago;
+        float valordevenda;
+        DateTime datadacompra;
+
+        //Opcionais
+        string tamanho;
+        bool condição;
+        string descricao;
+
+    }
+
+
+
+}
